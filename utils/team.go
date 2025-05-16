69225b01554f02408d48c84f0cce1f630d470500
LILLOLLAL <Gillbrian079@gmail.com>
2025-05-16 22:45:34 +0700

    Nambahin Beberapa Template

---
+++ utils/team.go
@@ -0,0 +1,15 @@
+package utils
+
+import "fmt"
+
+func Addteam(){
+	fmt.Println("Add Team Belum Jadi Rek")
+}
+
+func Modifyteam(){
+	fmt.Println("Modify Team belum jadi rek")
+}
+
+func DeleteTeam(){
+	fmt.Println("Delete Team Belum Jadi Rek")
+}
