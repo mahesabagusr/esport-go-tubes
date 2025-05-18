840e4e6e32ce26c5e0d2d07f3beb394488ed1d7e
mahesabagusr <mahesabagusraditya1@gmail.com>
2025-05-18 13:20:54 +0700

    feat : add team & players

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"esportgacor/database"
 	"esportgacor/handlers"
 	"esportgacor/utils"
 )
 
 func main() {
-
+	database.InitDB()
 	utils.DisplayMainMenu()
 	mainSelection := utils.DisplayOption()
 	handlers.HandleMainMenu(mainSelection)
